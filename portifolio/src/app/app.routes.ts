1bd7570357694f9ff596439aef246b7e123f9317
GraziellaSousa <105685287+GraziellaSousa@users.noreply.github.com>
2024-05-20 00:37:50 -0300

    finalizado

--- portifolio/src/app/app.routes.ts
+++ portifolio/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import{Carreira} from './carreira/carreira.component';
 import{HomeComponent} from './home/home.component';
-
+import{FormacaoComponent} from './formacao/formacao.component';
 export const routes: Routes = [
     {path: 'carreira', component: Carreira},
     {path: 'inicio', component: HomeComponent},
+    {path: 'formacao', component: FormacaoComponent},
     {path: '', redirectTo: '/home', pathMatch: 'full'},
 ];
